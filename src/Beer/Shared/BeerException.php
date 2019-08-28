4ed83e8fb293041df9106625d4c90cbb5ad0d16f
Tomasz Świacko-Świackiewicz <tomasz.swiacko-swiackiewicz@getresponse.com>
2019-08-28 23:07:29 +0200

    New named constructor added for disqus BeerComments implementaion

--- src/Beer/Shared/BeerException.php
+++ src/Beer/Shared/BeerException.php
@@ -6,16 +6,22 @@ namespace TSwiackiewicz\PHPersSummit\Beer\Shared;
 
 class BeerException extends \Exception
 {
-    private const NOT_ALLOWED_FOR_NOT_PREMIUM_USERS = 1234;
+    private const NOT_ALLOWED_FOR_NOT_ADMIN_USERS = 1234;
     private const RATING_FAILURE = 2345;
+    private const COMMENT_FAILURE = 3456;
 
-    public static function notAllowedForNotPremiumUsers(): self
+    public static function notAllowedForNotAdminUsers(): self
     {
-        return new self('Not allowed for not premium users', self::NOT_ALLOWED_FOR_NOT_PREMIUM_USERS);
+        return new self('Not allowed for not admin users', self::NOT_ALLOWED_FOR_NOT_ADMIN_USERS);
     }
 
     public static function ratingFailure(BeerId $beerId): self
     {
         return new self("Unable to rate beer {(string)$beerId}", self::RATING_FAILURE);
+    }
+
+    public static function commentFailure(BeerId $beerId): self
+    {
+        return new self("Unable to comment beer {(string)$beerId}", self::COMMENT_FAILURE);
     }
 }
